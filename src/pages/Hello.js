69daaab604fb5909f1e89df01197a696bba7cf97
unknown <miroyeh1986@gmail.com>
2023-04-13 11:15:35 +0800

    index links

--- src/pages/Hello.js
+++ src/pages/Hello.js
@@ -1,5 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Hello() {
-  return <h1>Hello World!</h1>;
+  return (
+    <div className="dicom-info">
+      <h1>Cornerstone Viewer Examples</h1>
+      <Link to="/viewer">Single Web Image</Link>
+      <Link to="/dicomviewer">Single Dicom Image</Link>
+      <Link to="/dicomviewerscroll">Dicom Image Stack</Link>
+      <Link to="/dicomviewerstacktool">Dicom Image Tools</Link>
+      <Link to="/cornerstoneajax">Dicom Image Ajax </Link>
+      <Link to="/webajax">Web Image Ajax</Link>
+    </div>
+  );
 }
